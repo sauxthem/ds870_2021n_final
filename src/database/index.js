3ad3d5dfab32a7a0d9b9e196631cd40f68a02520
Matheus Ribeiro <augustomoraes@ufpr.br>
2021-07-10 13:53:05 -0300

    Update database/index.js with proper model inits

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,21 @@
 const sequelize = require("sequelize");
 const dbConfig = require("./config/dbconfig");
 
+const Associate = require("../models/Associate");
+const Customer = require("../models/Customer");
+const Delivery = require("../models/Delivery");
+const Deliveryman = require("../models/Deliveryman");
+
 const conn = new sequelize(dbConfig)
+
+Associate.init(conn);
+Customer.init(conn);
+Delivery.init(conn);
+Deliveryman.init(conn);
+
+Associate.associate(conn.models);
+Customer.associate(conn.models);
+Delivery.associate(conn.models);
+Deliveryman.associate(conn.models);
 
 module.exports = conn;
