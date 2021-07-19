ae6cc8b18140b82517f891562f75010b2abf6168
Matheus Ribeiro <augustomoraes@ufpr.br>
2021-07-19 19:28:32 -0300

    Remove unused "login" files

--- src/routes/loginRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const loginRouter = express.Router();
-const loginController = require("../controllers/loginController");
-const auth = require("../middlewares/auth");
-
-loginRouter.post("/", loginController.login);
-loginRouter.get("/logout", auth, loginController.logout);
-
-module.exports = loginRouter;
